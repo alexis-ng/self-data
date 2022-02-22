7bba0b9526a744e49b34e3c58cbff918e5f057cd
alexis <alexism.ng26@gmail.com>
2022-02-22 08:56:50 -0600

    new stuff

--- self_pictures_resize/resize.py
+++ self_pictures_resize/resize.py
@@ -1,10 +1,10 @@
 from PIL import Image
 import pathlib
-maxsize = (272, 180)
-for input_img_path in pathlib.Path("new").iterdir():
-    output_img_path = str(input_img_path).replace("new","new")
+maxsize = (180, 272) #(width, height)
+for input_img_path in pathlib.Path("sienna").iterdir():
+    output_img_path = str(input_img_path).replace("sienna","edited") #old folder, new folder
     with Image.open(input_img_path) as im:
         im.thumbnail(maxsize, Image.ANTIALIAS)
-        im = im.rotate(90, expand = 1)
+        # im = im.rotate(90, expand = 1) #sometimes you need it, sometimes you don't
         im.save(output_img_path, "JPEG", dpi=(300,300))
         print(f"processing file {input_img_path} done...")
