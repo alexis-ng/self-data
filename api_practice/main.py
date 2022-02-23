a273046963a9df0fe42bc02856a8974a4f943f9b
alexis <alexism.ng26@gmail.com>
2022-02-23 08:38:03 -0600

    Co-authored-by: sng02 <sng02@users.noreply.github.com>

--- api_practice/main.py
+++ api_practice/main.py
@@ -1,9 +1,24 @@
+import uvicorn
+from fastapi import FastAPI
+from enum import Enum
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+app = FastAPI()
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
-#testing
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
