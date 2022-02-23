0313e6c54ba463cbef9ff0dff0cd2a1b7d596bc2
alexis <alexism.ng26@gmail.com>
2022-02-23 08:54:37 -0600

    post

--- api_practice/main.py
+++ api_practice/main.py
@@ -1,24 +1,21 @@
 import uvicorn
+from typing import Optional
 from fastapi import FastAPI
-from enum import Enum
+from pydantic import BaseModel
 
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
+class Item(BaseModel):
+    name : str
+    description : Optional[str] = None
+    price : float
+    tax: Optional[float] = None
+
 
 app = FastAPI()
 
-from fastapi import FastAPI
 
-app = FastAPI()
-
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
-
-
-@app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip : skip + limit]
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
